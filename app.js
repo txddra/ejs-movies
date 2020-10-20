c22a1d87e50f115a6c51e5b0f34b961a22fd8640
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-10-20 01:14:08 -0400

    added key

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/movies', require('./routes/movieRoutes'));
+
 const port = 3002;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,3 +1,15 @@
 const router = require('express').Router();
-const axios = require('axios')
+const axios = require('axios');
 
+const key = require('./key.js');
+
+router.get('/url', (req, res) => {
+    axios.get(`https://api.themoviedb.org/3/movie/550?api_key=${key}`)
+        .then((results) => {
+            // console.log(Array.isArray(results.data))
+           console.log(results.data);
+            return res.render('main/url', {
+                myData: myData
+            })
+        })
+});
