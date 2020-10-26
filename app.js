c95bde0a89249cdb65bc802026b5909ce95ebae3
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-10-26 02:42:58 -0400

    refresh

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+
+require('dotenv').config();
 //set the view for express
 app.set('view engine', 'ejs');
 //views are located
@@ -11,9 +13,11 @@ app.set('views', path.join(__dirname, 'views'));
 //use static assets
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/movies', require('./routes/movieRoutes'));
+app.use('/ejs', require('./routes/movieRoutes'));
 
 const port = 3002;
+
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,15 +1,30 @@
 const router = require('express').Router();
 const axios = require('axios');
 
-const key = require('./key.js');
+const key = require('../key.js');
 
-router.get('/url', (req, res) => {
+
+
+
+router.get('/', (req, res) => {
+    // res.send('Now its time for the view');
+    //render the view, 1st argument is the file
+    // 2nd argument is an object
+    res.render('movies');
+  
+  });
+
+
+router.get('/movies', (req, res) => {
     axios.get(`https://api.themoviedb.org/3/movie/550?api_key=${key}`)
         .then((results) => {
             // console.log(Array.isArray(results.data))
-           console.log(results.data);
-            return res.render('main/url', {
+            // console.log(results);
+            let myData =results.data;
+            return res.render('main/movies', {
                 myData: myData
             })
         })
 });
+
+module.exports =router
